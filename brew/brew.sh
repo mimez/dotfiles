6d3a68496c2197a2dcc18cad21c5f1f0db1bb226
Michael Mezger <michael@michaelmezger.de>
2021-12-14 19:08:41 +0100

    Update brew.sh

--- brew/brew.sh
+++ brew/brew.sh
@@ -1,2 +1,2 @@
 brew install colordiff php telnet postgresql coreutils node
-brew install --cask google-chrome iterm2 spotify microsoft-office visual-studio-code alfred whatsapp dropbox discord skitch tunnelblick evernote hot docker
+brew install --cask google-chrome iterm2 spotify microsoft-office visual-studio-code alfred whatsapp dropbox discord skitch tunnelblick evernote hot docker slack thunderbird
